0b60504cdb11cdb31e5c38903f7e6c5f5990c8f2
Ikenna <enebret@yahoo.com>
2023-08-10 11:26:03 +0100

    redacts

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -37,9 +37,26 @@ function Dashboard () {
     const [btcTwentyFourHigh, newBtcTwentyFourHigh] = useState(null);
     const [btcTwentyFourLow, newBtcTwentyFourLow] = useState(null);
     const [btcmarketCap, newBtcMarketCap] = useState(null);
-    const [th, newTH] = useState(null);
-    const [ada, newAda] = useState(null);
-    const [xrp, newXRP] = useState(null);
+    const [eth, newEth] = useState(null);
+    const [ethLogo, newEthLogo] = useState(null);
+    const [ethTwentyFourHigh, newEthTwentyFourHigh] = useState(null);
+    const [ethTwentyFourLow, newEthTwentyFourLow] = useState(null);
+    const [ethmarketCap, newEthMarketCap] = useState(null);
+    const [xrp, newXrp] = useState(null);
+    const [xrpLogo, newXrpLogo] = useState(null);
+    const [xrpTwentyFourHigh, newXrpTwentyFourHigh] = useState(null);
+    const [xrpTwentyFourLow, newXrpTwentyFourLow] = useState(null);
+    const [xrpmarketCap, newXrpMarketCap] = useState(null);
+    const [sol, newSol] = useState(null);
+    const [solLogo, newSolLogo] = useState(null);
+    const [solTwentyFourHigh, newSolTwentyFourHigh] = useState(null);
+    const [solTwentyFourLow, newSolTwentyFourLow] = useState(null);
+    const [solmarketCap, newSolMarketCap] = useState(null);
+    const [usdt, newUsdt] = useState(null);
+    const [usdtLogo, newUsdtLogo] = useState(null);
+    const [usdtTwentyFourHigh, newUsdtTwentyFourHigh] = useState(null);
+    const [usdtTwentyFourLow, newUsdtTwentyFourLow] = useState(null);
+    const [usdtmarketCap, newUsdtMarketCap] = useState(null);
     var loggedInUser = localStorage.getItem('user');
     var bal = localStorage.getItem('bal');
     async function getCoinData () {
@@ -48,9 +65,9 @@ function Dashboard () {
         currency: 'USD',
     });
       const btcReturnedPrice = await axios.get(url+"bitcoin?tickers=true");
-            const btcPrice = parseFloat(btcReturnedPrice.data.market_data.current_price.usd);
             const btcLogo = btcReturnedPrice.data.image.small;
             newBtcLogo(btcLogo);
+            const btcPrice = parseFloat(btcReturnedPrice.data.market_data.current_price.usd);
             newBtc(USDollar.format(btcPrice));
             const twentyFourHourHigh = btcReturnedPrice.data.market_data.high_24h.usd;
             newBtcTwentyFourHigh(USDollar.format(twentyFourHourHigh))
@@ -59,7 +76,54 @@ function Dashboard () {
             const marketCAp = btcReturnedPrice.data.market_data.market_cap.usd;
             newBtcMarketCap(USDollar.format(marketCAp))
             //const totalVol = btcReturnedPrice.data.market_data.total_volume.usd;
-            
+            const ethReturnedData = await axios.get(url+"ethereum?tickers=true");
+            const ethLogo = ethReturnedData.data.image.small;
+            newEthLogo(ethLogo);
+            const ethPrice = parseFloat(ethReturnedData.data.market_data.current_price.usd);
+            newEth(USDollar.format(ethPrice));
+            const ethDataTwentyFourHourHigh = ethReturnedData.data.market_data.high_24h.usd;
+            newEthTwentyFourHigh(USDollar.format(ethDataTwentyFourHourHigh))
+            const ethDataTwentyFourHourLow = ethReturnedData.data.market_data.low_24h.usd;
+            newEthTwentyFourLow(USDollar.format(ethDataTwentyFourHourLow))
+            const ethDatamarketCAp = ethReturnedData.data.market_data.market_cap.usd;
+            newEthMarketCap(USDollar.format(ethDatamarketCAp))
+        ///////////////////////////////////////////////////////////////////////////////////////////
+        const xrpReturnedData = await axios.get(url+"ripple?tickers=true");
+        const xrpLogo = xrpReturnedData.data.image.small;
+        newXrpLogo(xrpLogo);
+        const xrpPrice = parseFloat(xrpReturnedData.data.market_data.current_price.usd);
+        newXrp(USDollar.format(xrpPrice));
+        const xrpDataTwentyFourHourHigh = xrpReturnedData.data.market_data.high_24h.usd;
+        newXrpTwentyFourHigh(USDollar.format(xrpDataTwentyFourHourHigh))
+        const xrpDataTwentyFourHourLow = xrpReturnedData.data.market_data.low_24h.usd;
+        newXrpTwentyFourLow(USDollar.format(xrpDataTwentyFourHourLow))
+        const xrpDatamarketCAp = xrpReturnedData.data.market_data.market_cap.usd;
+        newXrpMarketCap(USDollar.format(xrpDatamarketCAp))
+      //////////////////////////////////////////////////////////////////////////////////////////////
+      const solReturnedData = await axios.get(url+"solana?tickers=true");
+      const solLogo = solReturnedData.data.image.small;
+      newSolLogo(solLogo);
+      const solPrice = parseFloat(solReturnedData.data.market_data.current_price.usd);
+      newSol(USDollar.format(solPrice));
+      const solDataTwentyFourHourHigh = solReturnedData.data.market_data.high_24h.usd;
+      newSolTwentyFourHigh(USDollar.format(solDataTwentyFourHourHigh))
+      const solDataTwentyFourHourLow = solReturnedData.data.market_data.low_24h.usd;
+      newSolTwentyFourLow(USDollar.format(solDataTwentyFourHourLow))
+      const solDatamarketCAp = solReturnedData.data.market_data.market_cap.usd;
+      newSolMarketCap(USDollar.format(solDatamarketCAp))
+      /////////////////////////////////////////////////////////////////////////////////////////////
+      const usdtReturnedData = await axios.get(url+"tether?tickers=true");
+      const usdtLogo = usdtReturnedData.data.image.small;
+      newUsdtLogo(usdtLogo);
+      const usdtPrice = parseFloat(usdtReturnedData.data.market_data.current_price.usd);
+      newUsdt(USDollar.format(usdtPrice));
+      const usdtDataTwentyFourHourHigh = usdtReturnedData.data.market_data.high_24h.usd;
+      newUsdtTwentyFourHigh(USDollar.format(usdtDataTwentyFourHourHigh))
+      const usdtDataTwentyFourHourLow = usdtReturnedData.data.market_data.low_24h.usd;
+      newUsdtTwentyFourLow(USDollar.format(usdtDataTwentyFourHourLow))
+      const usdtDatamarketCAp = usdtReturnedData.data.market_data.market_cap.usd;
+      newUsdtMarketCap(USDollar.format(usdtDatamarketCAp))
+
 
     };
     getCoinData()
@@ -71,15 +135,60 @@ function Dashboard () {
         };
         async function getApi (){
           try{
-            const adaReturnedPrice = await axios.get(url+"ADAUSDT");
-            const adaPrice = parseFloat(adaReturnedPrice.data.price);
-            newAda(adaPrice.toFixed(2));
-            const btcReturnedPrice = await axios.get(url+"bitcoin?tickers=true");
+            let USDollar = new Intl.NumberFormat('en-US', {
+              style: 'currency',
+              currency: 'USD',
+          });
+           const btcReturnedPrice = await axios.get(url+"bitcoin?tickers=true");
             const btcPrice = parseFloat(btcReturnedPrice.data.market_data.current_price.usd);
-            newBtc(btcPrice.toFixed(2));
-            const xrpReturnedPrice = await axios.get(url+"XRPUSDT");
-            const xrpPrice = parseFloat(xrpReturnedPrice.data.price);
-            newXRP(xrpPrice.toFixed(2));
+            newBtc(USDollar.format(btcPrice));
+            const twentyFourHourHigh = btcReturnedPrice.data.market_data.high_24h.usd;
+            newBtcTwentyFourHigh(USDollar.format(twentyFourHourHigh))
+            const twentyFourHourLow = btcReturnedPrice.data.market_data.low_24h.usd;
+            newBtcTwentyFourLow(USDollar.format(twentyFourHourLow))
+            const marketCAp = btcReturnedPrice.data.market_data.market_cap.usd;
+            newBtcMarketCap(USDollar.format(marketCAp))
+            //////////////////////////////////////////////////////////////////////
+            const ethReturnedData = await axios.get(url+"ethereum?tickers=true");
+            const ethPrice = parseFloat(ethReturnedData.data.market_data.current_price.usd);
+            newEth(USDollar.format(ethPrice));
+            const ethDataTwentyFourHourHigh = ethReturnedData.data.market_data.high_24h.usd;
+            newEthTwentyFourHigh(USDollar.format(ethDataTwentyFourHourHigh))
+            const ethDataTwentyFourHourLow = ethReturnedData.data.market_data.low_24h.usd;
+            newEthTwentyFourLow(USDollar.format(ethDataTwentyFourHourLow))
+            const ethDatamarketCAp = ethReturnedData.data.market_data.market_cap.usd;
+            newEthMarketCap(USDollar.format(ethDatamarketCAp))
+            /////////////////////////////////////////////////////////////////////////
+            const xrpReturnedData = await axios.get(url+"ripple?tickers=true");
+            const xrpPrice = parseFloat(xrpReturnedData.data.market_data.current_price.usd);
+            newXrp(USDollar.format(xrpPrice));
+            const xrpDataTwentyFourHourHigh = xrpReturnedData.data.market_data.high_24h.usd;
+            newXrpTwentyFourHigh(USDollar.format(xrpDataTwentyFourHourHigh))
+            const xrpDataTwentyFourHourLow = xrpReturnedData.data.market_data.low_24h.usd;
+            newXrpTwentyFourLow(USDollar.format(xrpDataTwentyFourHourLow))
+            const xrpDatamarketCAp = xrpReturnedData.data.market_data.market_cap.usd;
+            newXrpMarketCap(USDollar.format(xrpDatamarketCAp))
+            ///////////////////////////////////////////////////////////////////////////////
+            const  solReturnedData = await axios.get(url+"solana?tickers=true");
+            const solPrice = parseFloat(solReturnedData.data.market_data.current_price.usd);
+            newSol(USDollar.format(solPrice));
+            const solDataTwentyFourHourHigh = solReturnedData.data.market_data.high_24h.usd;
+            newSolTwentyFourHigh(USDollar.format(solDataTwentyFourHourHigh))
+            const solDataTwentyFourHourLow = solReturnedData.data.market_data.low_24h.usd;
+            newSolTwentyFourLow(USDollar.format(solDataTwentyFourHourLow))
+            const solDatamarketCAp = ethReturnedData.data.market_data.market_cap.usd;
+            newSolMarketCap(USDollar.format(solDatamarketCAp))
+            ///////////////////////////////////////////////////////////////////////////////
+            const usdtReturnedData = await axios.get(url+"tether?tickers=true");
+            const usdtPrice = parseFloat(usdtReturnedData.data.market_data.current_price.usd);
+            newUsdt(USDollar.format(usdtPrice));
+            const usdtDataTwentyFourHourHigh = usdtReturnedData.data.market_data.high_24h.usd;
+            newUsdtTwentyFourHigh(USDollar.format(usdtDataTwentyFourHourHigh))
+            const usdtDataTwentyFourHourLow = usdtReturnedData.data.market_data.low_24h.usd;
+            newUsdtTwentyFourLow(USDollar.format(usdtDataTwentyFourHourLow))
+            const usdtDatamarketCAp = usdtReturnedData.data.market_data.market_cap.usd;
+            newUsdtMarketCap(USDollar.format(usdtDatamarketCAp))
+      
           }
           catch (error) {
             console.error(error)
@@ -140,12 +249,36 @@ function Dashboard () {
           <td>{btcmarketCap}</td>
           </tr>
           <tr>
-          <td>cardano</td>
-           <td>{ada}</td>
+          <td><Image src={ethLogo} rounded /></td>
+          <td> <span style={{ fontWeight: 'bold'}}>Ethereum</span>ETH</td>
+          <td>{eth}</td>
+          <td>{ethTwentyFourHigh}</td>
+          <td>{ethTwentyFourLow}</td>
+          <td>{ethmarketCap}</td>
           </tr>
           <tr>
-          <td>ripple</td>
-           <td>{xrp}</td>
+          <td><Image src={xrpLogo} rounded /></td>
+          <td> <span style={{ fontWeight: 'bold'}}>Ripple</span>XRP</td>
+          <td>{xrp}</td>
+          <td>{xrpTwentyFourHigh}</td>
+          <td>{xrpTwentyFourLow}</td>
+          <td>{xrpmarketCap}</td>
+          </tr>
+          <tr>
+          <td><Image src={solLogo} rounded /></td>
+          <td> <span style={{ fontWeight: 'bold'}}>Solana</span>SOL</td>
+          <td>{sol}</td>
+          <td>{solTwentyFourHigh}</td>
+          <td>{solTwentyFourLow}</td>
+          <td>{solmarketCap}</td>
+          </tr>
+          <tr>
+          <td><Image src={usdtLogo} rounded /></td>
+          <td> <span style={{ fontWeight: 'bold'}}>Tether</span>USDT</td>
+          <td>{usdt}</td>
+          <td>{usdtTwentyFourHigh}</td>
+          <td>{usdtTwentyFourLow}</td>
+          <td>{usdtmarketCap}</td>
           </tr>
       </tbody>
           </Table>
